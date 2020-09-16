dfb16ac4224c499b7851b28d96b443df8c31bbe7
yui728 <yui.nishimra.1984@gmail.com>
2020-09-16 23:56:45 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/gettingstartedwebapp/GettingStartedWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.gettingstartedwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gettingstartedwebapp/GettingStartedWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gettingstartedwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
